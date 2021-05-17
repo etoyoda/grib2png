c7ca3ff93388b967819a06de72ede54ba76d27d9
TOYODA Eizi <toyoda.eizi@gmail.com>
2021-05-17 00:23:11 +0000

    要素、予報時間、高度の抽出ができるようになった

--- mainlogic.c
+++ mainlogic.c
@@ -1,29 +1,132 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <limits.h>
+#include <math.h>
 #include "gribscan.h"
 
 /*
  * 4 オクテットでパラメタを次の構造で表現する
+ * 構造:
  * 0xFF000000: 予約 (暫定ゼロ、ローカルの処理に使用)
  * 0x00FF0000: discipline
  * 0x0000FF00: category
  * 0x000000FF: parameter
  * ~0: エラー
+ * 用例:
+ * 0x00000000: temperature
+ * 0x00000101: relative humidity
+ * 0x00000108: total precipitation
+ * 0x00000202: u component of wind
+ * 0x00000203: v component of wind
+ * 0x0000020c: relative vorticity
+ * 0x0000020d: relative divergence
+ * 0x00000301: sea level pressure
+ * 0x00000305: geopotential height
  */
   unsigned long
 get_parameter(const struct grib2secs *gsp)
 {
+  unsigned pdst;
   unsigned long r;
   r = gsp->discipline << 16;
   if (gsp->pdslen == 0)
     return ~0;
-  switch (ui2(gsp->pds + 7)) {
+  pdst = ui2(gsp->pds + 7);
+  switch (pdst) {
   case 0:
+  case 8:
     r |= ui2(gsp->pds + 9);
     break;
   default:
+    fprintf(stderr, "unsupported PDS template 4.%u\n", pdst);
     return ~0;
+  }
+  return r;
+}
+
+  long
+get_ftime(const struct grib2secs *gsp)
+{
+  long r;
+  unsigned tunits;
+  unsigned pdst;
+  if (gsp->pdslen == 0)
+    return LONG_MAX;
+  pdst = ui2(gsp->pds + 7);
+  switch (pdst) {
+  case 0:
+  case 8:
+    tunits = gsp->pds[17];
+    r = ui4(gsp->pds + 18);
+    break;
+  default:
+    fprintf(stderr, "unsupported PDS template 4.%u\n", pdst);
+    return LONG_MAX;
+  }
+  switch (tunits) {
+  case 0: break;
+  case 1: r *= 60; break;
+  case 2: r *= (60 * 24); break;
+  case 10: r *= (3 * 60); break;
+  case 11: r *= (6 * 60); break;
+  case 12: r *= (12 * 60); break;
+  case 13: r /= 60; break;
+  default:
+    return LONG_MAX;
+  }
+  return r;
+}
+
+  double
+float40(const unsigned char *ptr)
+{
+  if (ptr[0] == 255) {
+    return nan("");
+  } else if ((0x80 & ptr[0]) != 0) {
+    return pow(10.0, ptr[0] & 0x7F) * ui4(ptr + 1);
+  } else {
+    return pow(10.0, -ptr[0]) * ui4(ptr + 1);
+  }
+}
+
+  double
+get_vlevel(const struct grib2secs *gsp)
+{
+  double r;
+  unsigned pdst, vtype;
+  const unsigned char *vlevptr;
+  if (gsp->pdslen == 0)
+    return LONG_MAX;
+  pdst = ui2(gsp->pds + 7);
+  switch (pdst) {
+  case 0:
+  case 8:
+    vtype = gsp->pds[22];
+    vlevptr = gsp->pds + 23;
+    break;
+  default:
+    fprintf(stderr, "unsupported PDS template 4.%u\n", pdst);
+    return nan("");
+    break;
+  }
+  switch (vtype) {
+  case 1:
+    r = 101325.0;
+    break;
+  case 100:
+    r = float40(vlevptr);
+    break;
+  case 101:
+    r = 101324.25;
+    break;
+  case 103:
+    r = 101324.0 - float40(vlevptr);
+    break;
+  default:
+    fprintf(stderr, "unsupported vertical level type %u\n", vtype);
+    return nan("");
+    break;
   }
   return r;
 }
@@ -35,7 +138,9 @@ checksec7(const struct grib2secs *gsp)
   char sreftime[24];
   get_reftime(&t, gsp);
   showtime(sreftime, sizeof sreftime, &t);
-  printf("%p %s %08lx\n", gsp->ds, sreftime, get_parameter(gsp));
+  printf("%p b%s p%08lx f%-+5ld v%-7.1lf\n",
+    gsp->ds, sreftime, get_parameter(gsp),
+    get_ftime(gsp), get_vlevel(gsp));
   return GSE_OKAY;
 }
 
