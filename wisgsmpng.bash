cd0ff5bbbe3ef8c310602fb3c088b19ba73a6815
TOYODA Eizi <toyoda.eizi@gmail.com>
2021-05-27 00:16:54 +0000

    WIS の syn アプリがファイル設置後短時間しかファイル存在を告知しないので、仕方がないからファイル名決め打ちで落とすことにした。

--- wisgsmpng.bash
+++ wisgsmpng.bash
@@ -1,53 +1,35 @@
 #!/bin/bash
 set -Ceuo pipefail
 # (C) TOYODA Eizi, 2021
+# GSM 予報値GRIBをダウンロードして画像変換する
 #
 PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin
 LANG=C
 TZ=UTC
 
-SYNURL='https://www.wis-jma.go.jp/data/syn?ContentType=Text&Resolution=1.25%201.25&Grid=Latitude/Longitude&Discipline=Global%20Spectral%20Model&Type=GRIB&Access=Open&Indicator=RJTD&Area=90.0%20-90.0%200.0%20358.75'
-
 test -d /nwp/p1/jmagrib || mkdir /nwp/p1/jmagrib
 cd /nwp/p1/jmagrib
 
-# syn.txt works as a lock file
-# just in case the script crashes and old one remains
-if lastrun=$(stat --format=%Z syn.txt 2>/dev/null) ; then
-  limit=$(date --date='6 hour ago' '+%s')
-  if [[ $lastrun -lt $limit ]] ; then
-    rm -f syn.txt
-    date --date="@${lastrun}" +'Lock file at %c - removed'
-  else
-    date --date="@${lastrun}" +'Lock file at %c - aborted'
-    false
-  fi
-fi
-trap "rm -f ${PWD}/syn.txt" EXIT
-wget -Osyn.txt -q $SYNURL
+# 推定最新時刻
+set $(LANG=C TZ=UTC date --date='3 hours ago' +'%Y%m%d %H')
+: date $*
+ymd=$1
+hh=$2
+let 'hh = hh / 6 * 6' || :
+hh=$(printf '%02u' $hh)
+# 推定最新時刻フォルダができていれば終了
+test ! -d ${ymd}T${hh}Z
 
-rm -f syn.out
-ruby - syn.txt <<RUBY >syn.out
-hit=nil
-while line=gets
-  line.chomp!
-  w=line.split(/\//)[-3..-1]
-  w.push line
-  next unless /A_SZ/ === w[2]
-  if hit.nil? then hit=w
-  elsif w[2]>hit[2] then hit=w
-  end
-end
-exit 1 unless hit
-puts [hit[0], 'T', hit[1][0..1], ' ', hit[3]].join
-RUBY
+mkdir ${ymd}T${hh}Z
+cd ${ymd}T${hh}Z
 
-read reftime url < syn.out
-rm -f syn.out
-test ! -d ${reftime}
-# exits if reftime already processed
 
-mkdir ${reftime}
-cd ${reftime}
-wget -q -Obig.grib "${url}"
-/nwp/bin/grib2png big.grib
+# syn がうまく動かないのでファイル決め打ちで攻める
+
+URL="https://www.wis-jma.go.jp/d/o/RJTD/GRIB/Global_Spectral_Model\
+/Latitude_Longitude/1.25_1.25/90.0_-90.0_0.0_358.75/Upper_air_layers\
+/${ymd}/${hh}0000/W_jp-JMA-tokyo,MODEL,JMA+gsm+gpv,C_RJTD_${ymd}${hh}0000\
+_GSM_GPV_Rgl_Gll1p25deg_L-all_FD0000-0512_grib2.bin"
+wget -q -Obiggrib.bin ${URL}
+
+/nwp/bin/grib2png biggrib.bin > grib2png.log
