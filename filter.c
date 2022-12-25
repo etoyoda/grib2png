729d5fe471777011ebcf614bfd4ed70dbd20c672
TOYODA Eizi <toyoda.eizi@gmail.com>
2022-12-25 21:43:28 +0900

    fix #24

--- filter.c
+++ filter.c
@@ -30,7 +30,7 @@ gribscan_filter(const char *sfilter,
     case '0': case '1': case '2': case '3': case '4':
     case '5': case '6': case '7': case '8': case '9':
       PUSH
-      *tos = strtod(sptr, (char **)&sptr);
+      *tos = strtod(sptr-1, (char **)&sptr);
       break;
     // immediate hexadecimal integer
     case 'x':
