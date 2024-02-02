04b9f24be497a62786ff824dd783f31c001b16fe
TOYODA Eizi <toyoda.eizi@gmail.com>
2024-02-02 00:02:31 +0000

    ファイル解読を進める

--- emagram.c
+++ emagram.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
   float p;
@@ -15,11 +16,50 @@ typedef struct {
   level_t *uv;
 } obs_t;
 
+size_t obs_size;
+obs_t *obs = NULL;
+
   int
-main(int argc, char **argv)
+obs_init(void)
 {
+  obs_size = 16;
+  obs = calloc(sizeof(obs_t), obs_size);
+  return 0;
+}
+
+  int
+loadfile(const char *fnam)
+{
+  FILE *fp;
+  char buf[1024];
+  fp=fopen(fnam, "rt");
+  if(fp==NULL) { perror(fnam); return -1; }
+  while (fgets(buf, sizeof buf, fp) != NULL) {
+    char *sreftime, *sparm, *sftime, *sdura, *svlev, *smemb, *sname, *sval;
+    sreftime = strtok(buf, ",");
+    sparm = strtok(NULL, ",");
+    sftime = strtok(NULL, ",");
+    sdura = strtok(NULL, ",");
+    svlev = strtok(NULL, ",");
+    smemb = strtok(NULL, ",");
+    sname = strtok(NULL, ",");
+    sval = strtok(NULL, ",\r\n");
+    printf("%s: %s: %s\n", sreftime, sftime, sval);
+  }
+  fclose(fp);
+  return 0;
+}
+
+  int
+main(int argc, const char **argv)
+{
+  obs_init();
   for (int i=1; i<argc; i++) {
-   
+    if (argv[i][0] == '-') {
+      fprintf(stderr, "unknown option %s\n", argv[i]);
+    } else {
+      loadfile(argv[i]);
+    }
   }
   return 0;
 }
