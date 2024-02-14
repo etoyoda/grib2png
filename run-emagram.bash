46e34d623859b3066b69273c638c8300116b30b0
TOYODA Eizi <toyoda.eizi@gmail.com>
2024-02-14 00:22:46 +0000

    地点再編

--- run-emagram.bash
+++ run-emagram.bash
@@ -67,13 +67,15 @@ test ! -f zobs.txt || rm -f zobs.txt
 ruby /nwp/bin/bufr2pick ${obsbf}:AHL=^IUKC61.RJTD.${dd}${hh} > zobs.txt
 test ! -f zmodel.txt || rm -f zmodel.txt
 /nwp/bin/gribpick -f'g360=' -p $gsmfile > zmodel.txt <<PICK
-42.50 141.25 42.5N141E_Shiraoi
-43.75 141.25 43.8N141E_Mashike
-45.00 141.25 45.0N141E_Rishiri
-41.25 141.25 41.3N141E_Mutsu
+40.00 141.25 +40+141/Iwate
+41.25 141.25 +41+141/Mutsu
+42.50 141.25 +43+141/Noboribetsu
+43.75 141.25 +44+141/Mashike
+45.00 141.25 +45+141/Rishiri
+45.25 141.25 +46+141/
 PICK
 test ! -f plot.png || rm -f plot.png
-/nwp/bin/emagram zmodel.txt zobs.txt
+/nwp/bin/emagram -s2 zmodel.txt zobs.txt
 mv -f plot.png ${yy}${mm}${dd}${hh}hokkaido.png
 
 # wajima
@@ -81,13 +83,16 @@ test ! -f zobs.txt || rm -f zobs.txt
 ruby /nwp/bin/bufr2pick ${obsbf}:AHL=^IUKC64.RJTD.${dd}${hh} > zobs.txt
 test ! -f zmodel.txt || rm -f zmodel.txt
 /nwp/bin/gribpick -f'g360=' -p $gsmfile > zmodel.txt <<PICK
-36.25 135.00 36.3N135.0E_Tango
-36.25 136.25 36.3N136.3E_Awara
-37.50 137.50 37.5N137.5E_Suzu
-38.75 138.75 38.8N138.8E_Sado
+33.75 135.00 +34+135/Gobo
+35.00 135.00 +35+135/Nishiwaki
+36.25 135.00 +36+135/Tango
+36.25 136.25 +36+136/Awara
+37.50 136.25 +38+136/Noto
+38.75 136.25 +39+136/
+40.00 136.25 +40+136/
 PICK
 test ! -f plot.png || rm -f plot.png
-/nwp/bin/emagram zmodel.txt zobs.txt
+/nwp/bin/emagram -s4 zmodel.txt zobs.txt
 mv -f plot.png ${yy}${mm}${dd}${hh}noto.png
 
 # kagoshima
@@ -95,49 +100,46 @@ test ! -f zobs.txt || rm -f zobs.txt
 ruby /nwp/bin/bufr2pick ${obsbf}:AHL=^IUKC70.RJTD.${dd}${hh} > zobs.txt
 test ! -f zmodel.txt || rm -f zmodel.txt
 /nwp/bin/gribpick -f'g360=' -p $gsmfile > zmodel.txt <<PICK
-31.25 130.0 31.3N130E_Makurazaki
-32.50 130.0 32.5N130E_Amakusa
-33.75 130.0 33.8N130E_Karatsu
-30.00 130.0 33.0N130E_Yaku
+28.75 130.0 +29+130/Amami
+30.00 130.0 +30+130/Yakushima
+31.25 130.0 +31+130/Makurazaki
+32.50 130.0 +33+130/Amakusa
+33.75 130.0 +34+130/Karatsu
+35.00 130.0 +35+130/Tsushima
+36.25 130.0 +36+130/Pohang
 PICK
 test ! -f plot.png || rm -f plot.png
-/nwp/bin/emagram zmodel.txt zobs.txt
+/nwp/bin/emagram -s2 zmodel.txt zobs.txt
 mv -f plot.png ${yy}${mm}${dd}${hh}kyushu.png
 
 # naze
 test ! -f zobs.txt || rm -f zobs.txt
-ruby /nwp/bin/bufr2pick ${obsbf}:AHL=^IUKC71.RJTD.${dd}${hh} > zobs.txt
+ruby /nwp/bin/bufr2pick "${obsbf}:AHL=^IUKC71.RJTD.${dd}${hh}" > zobs.txt
+ruby /nwp/bin/bufr2pick "${obsbf}:AHL=^IUKC72.RJTD.${dd}${hh}" >> zobs.txt
 test ! -f zmodel.txt || rm -f zmodel.txt
 /nwp/bin/gribpick -f'g360=' -p $gsmfile > zmodel.txt <<PICK
-28.75 130.00 28.8N130.0E_Amami
-27.50 128.75 27.5N128.8E_O.Erabu
-26.25 127.50 26.3N127.5E_Okinawa
-25.00 126.25 25.0N126.3E
+23.75 123.75 +24+124/Hateruma
+25.00 125.00 +25+125/Miyakojima
+25.00 126.25 +25+126/
+26.25 127.50 +26+128/Okinawa
+27.50 128.75 +28+129/Okinoerabu
+28.75 130.00 +29+130/Amami
 PICK
 test ! -f plot.png || rm -f plot.png
 /nwp/bin/emagram zmodel.txt zobs.txt
-mv -f plot.png ${yy}${mm}${dd}${hh}amami.png
-
-# ishigaki
-test ! -f zobs.txt || rm -f zobs.txt
-ruby /nwp/bin/bufr2pick ${obsbf}:AHL=^IUKC72.RJTD.${dd}${hh} > zobs.txt
-test ! -f zmodel.txt || rm -f zmodel.txt
-/nwp/bin/gribpick -f'g360=' -p $gsmfile > zmodel.txt <<PICK
-23.75 123.75 23.8N123.8E_Hateruma
-25.00 122.50 25.0N122.5E_Yonaguni
-25.00 125.00 25.0N125.0E_Miyakojima
-26.25 123.75 26.3N123.8E
-PICK
-test ! -f plot.png || rm -f plot.png
-/nwp/bin/emagram zmodel.txt zobs.txt
-mv -f plot.png ${yy}${mm}${dd}${hh}yaeyama.png
+mv -f plot.png ${yy}${mm}${dd}${hh}ryukyu.png
 
 # minamidaito
 test ! -f zobs.txt || rm -f zobs.txt
-ruby /nwp/bin/bufr2pick ${obsbf}:AHL=^IUKC73.RJTD.${dd}${hh} > zobs.txt
+ruby /nwp/bin/bufr2pick ${obsbf}:AHL=^IUKC71.RJTD.${dd}${hh} > zobs.txt
+ruby /nwp/bin/bufr2pick ${obsbf}:AHL=^IUKC73.RJTD.${dd}${hh} >> zobs.txt
 test ! -f zmodel.txt || rm -f zmodel.txt
 /nwp/bin/gribpick -f'g360=' -p $gsmfile > zmodel.txt <<PICK
-26.25 131.25 26.3N131.3N_Daito
+26.25 131.25 +26+131/Minamidaitojima
+27.50 130.00 +28+130/Kikai
+28.75 128.75 +29+129/
+30.00 127.50 +30+128/
+31.25 126.25 +31+126/
 PICK
 test ! -f plot.png || rm -f plot.png
 /nwp/bin/emagram zmodel.txt zobs.txt
@@ -149,11 +151,15 @@ ruby /nwp/bin/bufr2pick ${obsbf}:AHL=^IUKC73.RJTD.${dd}${hh} > zobs.txt
 ruby /nwp/bin/bufr2pick ${obsbf}:AHL=^IUSC03.RJTD.${dd}${hh} | grep ,47991 >> zobs.txt || :
 test ! -f zmodel.txt || rm -f zmodel.txt
 /nwp/bin/gribpick -f'g360=' -p $gsmfile > zmodel.txt <<PICK
-27.50 142.50 27.5N142.5E_Ogasawara
-25.00 153.75 25.0N153.8E_M.torishima
+10.00 142.50 +10+143/
+15.00 145.00 +15+145/Saipan
+20.00 145.00 +20+145/
+22.50 145.00 +23+145/
+25.00 153.75 +25+154/Minamitorishima
+27.50 142.50 +28+143/Ogasawara
 PICK
 test ! -f plot.png || rm -f plot.png
-/nwp/bin/emagram zmodel.txt zobs.txt
+/nwp/bin/emagram -s5 zmodel.txt zobs.txt
 mv -f plot.png ${yy}${mm}${dd}${hh}ogasawara.png
 
 # kanto
@@ -161,16 +167,26 @@ test ! -f zobs.txt || rm -f zobs.txt
 ruby /nwp/bin/bufr2pick ${obsbf}:AHL=^IUKC65.RJTD.${dd}${hh} > zobs.txt
 test ! -f zmodel.txt || rm -f zmodel.txt
 /nwp/bin/gribpick -f'g360=' -p $gsmfile > zmodel.txt <<PICK
-33.75 140 33.8N140E_Mikurajima
-35 140    35.0N140E_Tateyama
-36.25 140 36.3N140E_Chikusei
-37.5 140  37.5N140E_Aizu
-38.75 140 38.8N140E_Shonai
-40 140    40.0N140E_Ogata
+32.5  140 +33+140/Hachijojima
+33.75 140 +34+140/Miyakejima
+35 140    +35+140/Tateyama
+36.25 140 +36+140/Chikusei
+37.5 140  +38+140/Aizu
+38.75 140 +39+140/Shonai
+40 140    +40+140/Oogata
 PICK
 test ! -f plot.png || rm -f plot.png
-/nwp/bin/emagram zmodel.txt zobs.txt
+/nwp/bin/emagram -s3 zmodel.txt zobs.txt
 mv -f plot.png ${yy}${mm}${dd}${hh}kanto.png
+
+test ! -f zmodel.txt || rm -f zmodel.txt
+ls -1 /nwp/p1/jmagrib/2*/gsm*.bin | tail -7 > zstn.txt
+/nwp/bin/gribpick -f'g0=' -p $(cat zstn.txt) > zmodel.txt <<PICK
+  36.25 140 +36+140/Chikusei
+PICK
+test ! -f plot.png || rm -f plot.png
+/nwp/bin/emagram -s6 zmodel.txt zobs.txt
+mv -f plot.png ${yy}${mm}${dd}${hh}kanto2.png
 
 rm -f z*.txt
 cd ..
