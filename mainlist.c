48bf991dcfe38577fa2c00d61a1cd377f49b8322
TOYODA Eizi <toyoda@met.kishou.go.jp>
2025-01-23 00:16:27 +0000

    フィルタ機能を griblist にも実装

--- mainlist.c
+++ mainlist.c
@@ -3,15 +3,21 @@
 #include "gribscan.h"
 #include "mymalloc.h"
 
+// empty filter string means "accept all"
+static const char *sfilter = "";
+
 // gribscan ライブラリから呼び返される関数。
   gribscan_err_t
 checksec7(const struct grib2secs *gsp)
 {
+  gribscan_err_t r;
+  // dimensions
   struct tm t;
   char sreftime[24];
   unsigned long iparm;
   double vlev, memb;
   long ftime, dura;
+  r = GSE_OKAY;
   // retrieve PDT metadata
   get_reftime(&t, gsp);
   showtime(sreftime, sizeof sreftime, &t);
@@ -20,11 +26,26 @@ checksec7(const struct grib2secs *gsp)
   vlev = get_vlevel(gsp);
   dura = get_duration(gsp);
   memb = get_perturb(gsp);
+  // filter
+  switch (gribscan_filter(sfilter, iparm, ftime, dura, vlev, memb)) {
+    case ERR_FSTACK:
+    case GSE_SKIP:
+      goto END_SKIP;
+      break;
+    case GSE_OKAY:
+    default:
+      /* do nothing */;
+  }
   // display
   printf("b%s %6s f%-+5ld d%-+5ld v%-8s m%-+4.3g\n",
     sreftime, param_name(iparm), ftime, dura, level_name(vlev), memb);
+  goto END_NORMAL;
+
+END_SKIP:
+  r = GSE_SKIP;
+END_NORMAL:
   myfree(gsp->ds);
-  return GSE_OKAY;
+  return r;
 }
 
   gribscan_err_t
@@ -33,6 +54,15 @@ argscan(int argc, const char **argv)
   gribscan_err_t r = ERR_NOINPUT;
   for (int i=1; i<argc; i++) {
     if (argv[i][0]=='-') {
+      switch (argv[i][1]) {
+      case 'f':
+        sfilter = argv[i]+2;
+	break;
+      default:
+        fprintf(stderr, "%s: unknown option\n", argv[i]);
+	r = GSE_JUSTWARN;
+	goto BARF;
+      }
     } else {
       r = grib2scan_by_filename(argv[i]);
       if (r != GSE_OKAY) goto BARF;
