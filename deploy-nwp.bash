08013b37c937cc48a21419fe0e4a9fce68405159
TOYODA Eizi <toyoda.eizi@gmail.com>
2026-05-26 00:15:52 +0000

    インストーラ修正: /nwp/a1 がなければ作る

--- deploy-nwp.bash
+++ deploy-nwp.bash
@@ -4,4 +4,5 @@ do
   sudo -u nwp install -m 0755 $file /nwp/bin
 done
 sudo -u nwp install -m 0644 map.html /nwp/p1/
+test -d /nwp/a1 || sudo install -d -o nwp -g nwp -m 0755 /nwp/a1
 sudo install -m 0644 crontab.txt /etc/cron.d/grib2png
