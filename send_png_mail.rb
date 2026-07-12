20d85e1c022534fd059468e5a423c8fbe461cb42
TOYODA Eizi <toyoda.eizi@gmail.com>
2026-07-12 23:16:45 +0000

    sending HTML mail to display images

--- send_png_mail.rb
+++ send_png_mail.rb
@@ -21,45 +21,70 @@ ARGV.each do |arg|
     from=Regexp.last_match(1)
   when /\Ato=(.+)\z/
     to=Regexp.last_match(1)
-  when /\Asubj(:?ect)?=(.+)\z/
+  when /\Asubject=(.+)\z/
     subject=Regexp.last_match(1)
   else
     png_files.push(arg)
   end
 end
 abort("no png file given") if png_files.empty?
-subject |= File.basename(png_files.first)
+unless subject
+  subject = File.basename(png_files.first)
+end
 
 boundary="----=_Part_#{SecureRandom.hex(16)}"
+altbound="----=_Part_#{SecureRandom.hex(16)}"
 
 mail=[]
-mail << "From: #{from}\r\n"
-mail << "To: #{to}\r\n"
-mail << "Subject: #{subject}\r\n"
-mail << "MIME-Version: 1.0\r\n"
-mail << "Content-Type: multipart/mixed; boundary=#{boundary}\r\n"
-mail << "\r\n"
+mail << "From: #{from}"
+mail << "To: #{to}"
+mail << "Subject: #{subject}"
+mail << "MIME-Version: 1.0"
+mail << "Content-Type: multipart/mixed; boundary=#{boundary}"
+mail << ""
 
-mail << "--#{boundary}\r\n"
-mail << "Content-Type: text/plain; charset=UTF-8\r\n"
-mail << "Content-Transfer-Encoding: 7bit\r\n"
-mail << "\r\n"
-mail << "Please find attched files.\r\n"
-mail << "\r\n"
+mail << "--#{boundary}"
+mail << "Content-Type: multipart/alternative; boundary=\"#{altbound}\""
+mail << ""
 
+mail << "--#{altbound}"
+mail << "Content-Type: text/plain; charset=UTF-8"
+mail << "Content-Transfer-Encoding: 7bit"
+mail << ""
+mail << "Please find attched files:"
+mail << "#{png_files.inspect}"
+mail << ""
+
+mail << "--#{altbound}"
+mail << "Content-Type: text/html; charset=UTF-8"
+mail << "Content-Transfer-Encoding: 7bit"
+mail << ""
+mail << "<html><head><title>PNG sender</title></head><body>"
+mail << "<p>Please find attched files:</p>"
+png_files.size.times do |i|
+  mail << "<p><img src=\"cid:image#{i}\"></p>"
+end
+mail << "<body></html>"
+mail << ""
+
+mail << "--#{altbound}--"
+
+cidno=0
 png_files.each do |file|
   filename = File.basename(file)
   encoded = Base64.encode64(File.binread(file))
-  mail << "--#{boundary}\r\n"
-  mail << "Content-Type: image/png; name=\"#{filename}\"\r\n"
-  mail << "Content-Transfer-Encoding: base64\r\n"
-  mail << "Content-Disposition: attachment; filename=\"#{filename}\"\r\n"
-  mail << "\r\n"
+  mail << "--#{boundary}"
+  mail << "Content-Type: image/png; name=\"#{filename}\""
+  mail << "Content-Transfer-Encoding: base64"
+  mail << "Content-Disposition: attachment; filename=\"#{filename}\""
+  mail << "Content-ID: image#{cidno}"
+  mail << ""
   mail << encoded
-  mail << "\r\n"
+  mail << ""
+  cidno=cidno.succ
 end
-mail << "--#{boundary}--\r\n"
+mail << "--#{boundary}--"
 
 IO.popen("/usr/lib/sendmail -t", "w") do |sendmail|
-  sendmail.write(mail.join)
+  sendmail.write(mail.join("\r\n"))
 end
